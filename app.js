ea6f3c473c36578f98dbd6f5f37f40ca95728194
Anushil98 <anushilghoshdastidar@gmail.com>
2020-05-06 00:48:56 +0530

    new changes like session management is applied

--- app.js
+++ app.js
@@ -4,47 +4,56 @@ const mongoose = require('mongoose');
 
 const bodyparser = require('body-parser')
 
+const session = require('express-session')
+
+const MongoDbStore = require('connect-mongodb-session')(session)
+
 const cors = require('cors')
 
 var app = express()
+const MONGOURI = 'mongodb+srv://Anushil:7sPH6ssTWMtqnh7C@clusternode-rzps1.mongodb.net/test?retryWrites=true&w=majority'
+const store = new MongoDbStore({
+    uri: 'mongodb+srv://Anushil:7sPH6ssTWMtqnh7C@clusternode-rzps1.mongodb.net/test?retryWrites=true&w=majority',
+    collection:'sessions'
+})
+app.use(cors({credentials:true,origin:'http://localhost:3001'}))
 
-app.use(cors())
+app.use(session({secret:'codeacious-secret-node-project', resave: false, saveUninitialized: false,
+cookie:{secure:false},store:store}))
 
-// app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json())
 
 app.post('/signup',(req,res,next)=>{
-    res.json({
-        user_Logged:req.body.username
-    })
+    
 })
 
-app.post('/login',(req,res,next)=>{
+app.post('/user',(req,res,next)=>{
     console.log(req.body)
-
+    req.session.loggedIn = true
+    console.log(req.session)
+    res.end()
 })
 
-app.post('/addpost',(req,res,next)=>{
+app.post('/post',(req,res,next)=>{
     console.log(req)
 })
 
-app.use('/getpost',(req,res,next)=>{
-    console.log(req)
+app.get('/post',(req,res,next)=>{
+    console.log(req.session.loggedIn)
+    res.end()
 })
 
-app.post('/deletepost',(req,res,next)=>{
+app.delete('/post',(req,res,next)=>{
     console.log(req)
 })
 
 app.use('/',(req,res,next)=>{
-    console.log("rfghj")
-    res.status(404).json({
-        error: "Error"
-    })
+    res.status(404).write("<h1>Error reached no valid api end</h1>")
+    return res.end()
 })   
-
-mongoose.connect('mongodb+srv://Anushil:b15XwRIwgyMpTYii@clusternode-rzps1.mongodb.net/test?retryWrites=true&w=majority')
-.then((esult)=>{
+// 7sPH6ssTWMtqnh7C
+mongoose.connect('mongodb+srv://Anushil:7sPH6ssTWMtqnh7C@clusternode-rzps1.mongodb.net/test?retryWrites=true&w=majority')
+.then((result)=>{
     console.log("Connected to database")
     app.listen(3000)
 }).catch((err)=>{
